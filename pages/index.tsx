7e869310a15783d73fbbf35ee7e86e083d53edc0
Anusheel Bhushan <anusheel@users.noreply.github.com>
2023-11-08 20:09:51 -0800

    Commit from Stub AI (#2)
    Co-authored-by: anusheel <bot@codestub.ai>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import CounterButton from '../components/CounterButton';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between p-24 bg-yellow-300 ${inter.className}`}
     >
+      <CounterButton />
     </main>
   )
 }
